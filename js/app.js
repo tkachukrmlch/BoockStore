11e4df0cef0e40ef71b5b7e785006a43d18576d6
Roman <tkachurm.ovb@gmail.com>
2019-03-29 01:14:57 +0300

    boockstart

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".my-rating").starRating({
+        initialRating: 2,
+        strokeColor: '#ffab00',
+        strokeWidth: 10,
+        starSize: 20
+    });
+});
